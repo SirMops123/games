6d2ae17a23cb9b023c381b2d79c4b90ac99b42af
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-27 16:01:49 +0100

    feat(signals): add subscribers to signal

--- src/engine/utils/signal.ts
+++ src/engine/utils/signal.ts
@@ -1,20 +1,37 @@
 export function signal<T>(initial: T): Signal<T> {
+    const subscribers: SignalSubscription<T>[] = []
     let value = initial;
 
     function read() {
         return value;
     }
 
-    read.set = (v: T) => {
-        value = v;
+    read.set = (newValue: T) => {
+        for (const s of subscribers) {
+            s(value, newValue)
+        }
+
+        value = newValue;
     }
 
     read.update = (c: (v: T) => T) => {
-        value = c(value);
+        const newValue = c(value);
+
+        for (const s of subscribers) {
+            s(value, newValue)
+        }
+
+        value = newValue;
+    }
+
+    read.subscribe = (c: (v: T) => void) => {
+        subscribers.push(c);
     }
 
     return read;
 }
+
+export type SignalSubscription<T> = (newValue: T, oldValue: T) => void
 
 export type Signal<T> = {
     (): T;
