0757f5efbe15ab89b884fcb9c8f6fc9f9b807c7b
davo <170838562+daftvoid@users.noreply.github.com>
2026-04-09 20:17:57 +0200

    fix(config): fixed astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://htl-stp.github.io',
+	base: '/games/',
 	vite: {
 		resolve: {
 			alias: {
